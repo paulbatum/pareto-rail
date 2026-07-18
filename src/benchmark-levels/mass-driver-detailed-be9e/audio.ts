32e170578774e9cf7f1fefb0101032ea20e45c7f
Paul Batum <paul.batum@gmail.com>
2026-07-18 01:55:56 -0700

    complete mass driver combat sound responses

--- src/benchmark-levels/mass-driver-detailed-be9e/audio.ts
+++ src/benchmark-levels/mass-driver-detailed-be9e/audio.ts
@@ -10,6 +10,8 @@ export function createAudio(bus:EventBus){
  bus.on('spawn',({kind}:any)=>{if(kind==='interlock')act((c,m,t)=>{tone(c,m.sfx,t,56,.18,.32);tone(c,m.sfx,t+.12,63,.14,.28);});});
  bus.on('lock',({lockCount})=>act((c,m,t)=>tone(c,m.sfx,t,520+lockCount*110,.05,.08)));
  bus.on('fire',({volleySize})=>act((c,m,t)=>{tone(c,m.sfx,t,150,.1,.12+volleySize*.02);m.duckAt(t,.7,.12);}));
+ bus.on('hit',({lethal}:any)=>act((c,m,t)=>tone(c,m.sfx,t,lethal?940:610,lethal?.12:.06,lethal?.18:.08)));
+ bus.on('playerhit',()=>act((c,m,t)=>{tone(c,m.sfx,t,42,.2,.35);tone(c,m.sfx,t,78,.09,.18);}));
  bus.on('kill',({enemyId}:any)=>act((c,m,t)=>tone(c,m.sfx,t,700+(enemyId%6)*90,.16,.15)));
  bus.on('reject',()=>act((c,m,t)=>tone(c,m.sfx,t,78,.12,.12)));
  bus.on('miss',()=>act((c,m,t)=>tone(c,m.sfx,t,96,.08,.06)));
